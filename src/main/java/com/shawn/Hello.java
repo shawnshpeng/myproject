c55775c341387a54e41dc8bd2704f4a0b590bd07
Shawn Peng <shawn.sh.peng@gmail.com>
2019-07-08 02:23:11 +0800

    Java & Kotlin variable, data type

--- src/main/java/com/shawn/Hello.java
+++ src/main/java/com/shawn/Hello.java
@@ -5,5 +5,20 @@ public class Hello {
         // System.out.println("Hello world");
         Person person = new Person();
         person.hello();
+        /*
+         * 基本資料型態：byte short char int long float double boolean
+         * 參考資料型態：String
+         * 包裝類別：Byte Short Character Integer Long Float Double Boolean
+         */
+        int age = 19;
+        Integer age2 = 19;
+        char c = '我';
+        Character c2 = 'A';
+        byte b = 120;
+        float weight = 66.5f;
+        float height = 1.7f;
+        boolean adult = true;
+        boolean enroll = false;
+        String name = "Tom";
     }
 }
--- src/main/kotlin/com/shawn/Hello.kt
+++ src/main/kotlin/com/shawn/Hello.kt
@@ -4,6 +4,14 @@ fun main() {
     // println("Hello Kotlin")
     val human = Human()
     human.hello()
+    /*
+     * 參考資料型態：Byte Short Int Long Float Double Char Boolean String
+     */
+    var age = 19
+    age = 20
+    var weight = 66.5f
+    var name: String
+    name = "Shawn"
 }
 
 class Human {
