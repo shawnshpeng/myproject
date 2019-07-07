8af3d251268de650eb22301cd29a6ebaf2aeca45
Shawn Peng <shawn.sh.peng@gmail.com>
2019-07-07 23:56:46 +0800

    Initial commit

---
+++ src/main/java/com/shawn/Hello.java
@@ -0,0 +1,9 @@
+package com.shawn;
+
+public class Hello {
+    public static void main(String[] args) {
+        // System.out.println("Hello world");
+        Person person = new Person();
+        person.hello();
+    }
+}
---
+++ src/main/kotlin/com/shawn/Hello.kt
@@ -0,0 +1,13 @@
+package com.shawn
+
+fun main() {
+    // println("Hello Kotlin")
+    val human = Human()
+    human.hello()
+}
+
+class Human {
+    fun hello() {
+        println("Hello Kotlin")
+    }
+}
