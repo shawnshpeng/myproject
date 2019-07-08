833a929ed5347413f598b8b1cb28d214376cf212
Shawn Peng <shawn.sh.peng@gmail.com>
2019-07-09 04:08:32 +0800

    Java & Kotlin while

---
+++ src/main/java/com/shawn/guess/Game.java
@@ -0,0 +1,31 @@
+package com.shawn.guess;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Game {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int secret = random.nextInt(10) + 1;
+        System.out.println(secret);
+        Scanner scanner = new Scanner(System.in);
+        int number = 0;
+        while (number != secret) {
+            System.out.print("Please enter a number: ");
+            number = scanner.nextInt();
+            if (number < secret) {
+                System.out.println("higher");
+            } else if (number > secret) {
+                System.out.println("lower");
+            } else {
+                System.out.println("Great, the number is " + secret);
+            }
+        }
+
+        /*int i = 0;
+        do {
+            System.out.println(i);
+            i += 1;
+        } while (i < 3);*/
+    }
+}
