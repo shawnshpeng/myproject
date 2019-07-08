f82dc578bccc29335befd5d68d22e7b7904e2798
Shawn Peng <shawn.sh.peng@gmail.com>
2019-07-09 06:12:01 +0800

    Java & Kotlin jUnit 5

---
+++ src/test/java/com/shawn/StudentTest.java
@@ -0,0 +1,19 @@
+package com.shawn;
+
+import com.shawn.student.Student;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StudentTest {
+    @Test
+    public void highestScoreTest() {
+        Student student = new Student("Shawn", 60, 80);
+        Assertions.assertEquals(80, student.highest());
+    }
+
+    @Test
+    public void averageTest() {
+        Student student = new Student("Shawn", 60, 80);
+        Assertions.assertEquals((60 + 80) / 2, student.getAverage());
+    }
+}
---
+++ src/test/kotlin/com/shawn/PersonTest.kt
@@ -0,0 +1,20 @@
+package com.shawn.kotlin
+
+import com.shawn.Human
+import com.shawn.Person
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class PersonTest {
+    @Test
+    fun humanBmiTest() {
+        val human = Human("Shawn", 66.5f, 1.7f)
+        Assertions.assertEquals(66.5f / (1.7f * 1.7f), human.bmi())
+    }
+
+    @Test
+    fun personBmiTest() {
+        val person = Person(66.5f, 1.7f)
+        Assertions.assertEquals(66.5f / (1.7f * 1.7f), person.bmi())
+    }
+}
